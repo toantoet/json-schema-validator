5ee8222295136a5960bf59b2b1c98c6b18ceb772
Toan Nguyen <toantoet@gmail.com>
2016-07-14 16:27:03 +0700

    support timezone offset, optionally.

--- src/main/java/com/github/fge/jsonschema/format/common/DateTimeAttribute.java
+++ src/main/java/com/github/fge/jsonschema/format/common/DateTimeAttribute.java
@@ -27,6 +27,7 @@ import com.github.fge.jsonschema.format.FormatAttribute;
 import com.github.fge.jsonschema.processors.data.FullData;
 import com.github.fge.msgsimple.bundle.MessageBundle;
 import com.google.common.collect.ImmutableList;
+
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.DateTimeFormatterBuilder;
 import org.joda.time.format.DateTimeParser;
@@ -39,61 +40,57 @@ import static org.joda.time.DateTimeFieldType.*;
  * Validator for the {@code date-time} format attribute
  */
 public final class DateTimeAttribute
-    extends AbstractFormatAttribute
-{
+        extends AbstractFormatAttribute {
     private static final List<String> FORMATS = ImmutableList.of(
-        "yyyy-MM-dd'T'HH:mm:ssZ", "yyyy-MM-dd'T'HH:mm:ss.[0-9]{1,12}Z"
+            "yyyy-MM-dd'T'HH:mm:ssZ", "yyyy-MM-dd'T'HH:mm:ss.[0-9]{1,12}Z"
     );
     private static final DateTimeFormatter FORMATTER;
 
     static {
         final DateTimeParser secFracsParser = new DateTimeFormatterBuilder()
-            .appendLiteral('.').appendFractionOfSecond(1,12)
-            .toParser();
+                .appendLiteral('.').appendFractionOfSecond(1, 12)
+                .toParser();
 
         DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
 
         builder = builder.appendFixedDecimal(year(), 4)
-            .appendLiteral('-')
-            .appendFixedDecimal(monthOfYear(), 2)
-            .appendLiteral('-')
-            .appendFixedDecimal(dayOfMonth(), 2)
-            .appendLiteral('T')
-            .appendFixedDecimal(hourOfDay(), 2)
-            .appendLiteral(':')
-            .appendFixedDecimal(minuteOfHour(), 2)
-            .appendLiteral(':')
-            .appendFixedDecimal(secondOfMinute(), 2)
-            .appendOptional(secFracsParser)
-            .appendTimeZoneOffset("Z", false, 2, 2);
+                .appendLiteral('-')
+                .appendFixedDecimal(monthOfYear(), 2)
+                .appendLiteral('-')
+                .appendFixedDecimal(dayOfMonth(), 2)
+                .appendLiteral('T')
+                .appendFixedDecimal(hourOfDay(), 2)
+                .appendLiteral(':')
+                .appendFixedDecimal(minuteOfHour(), 2)
+                .appendLiteral(':')
+                .appendFixedDecimal(secondOfMinute(), 2)
+                .appendOptional(secFracsParser)
+                .appendTimeZoneOffset("Z", "", false, 2, 2);
 
         FORMATTER = builder.toFormatter();
     }
 
     private static final FormatAttribute INSTANCE = new DateTimeAttribute();
 
-    public static FormatAttribute getInstance()
-    {
+    public static FormatAttribute getInstance() {
         return INSTANCE;
     }
 
-    private DateTimeAttribute()
-    {
+    private DateTimeAttribute() {
         super("date-time", NodeType.STRING);
     }
 
     @Override
     public void validate(final ProcessingReport report,
-        final MessageBundle bundle, final FullData data)
-        throws ProcessingException
-    {
+                         final MessageBundle bundle, final FullData data)
+            throws ProcessingException {
         final String value = data.getInstance().getNode().textValue();
 
         try {
             FORMATTER.parseDateTime(value);
         } catch (IllegalArgumentException ignored) {
             report.error(newMsg(data, bundle, "err.format.invalidDate")
-                .putArgument("value", value).putArgument("expected", FORMATS));
+                    .putArgument("value", value).putArgument("expected", FORMATS));
         }
     }
 }
